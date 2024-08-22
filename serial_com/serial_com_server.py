c65f52bc8a782de832cbfdc75d18acea626248f5
asak200 <abdoorahmansk2004@gmail.com>
2024-08-22 19:54:38 +0300

    erfb

--- serial_com/serial_com_server.py
+++ serial_com/serial_com_server.py
@@ -46,20 +46,23 @@ class ComNode(Node):
             self.pub_enc.publish(msg)
 
     def listen(self):
+        self.get_logger().info("listen")
         try:
+            self.get_logger().info("try")
             while True:
-                time.sleep(0.01)
-                self.get_logger().info("asak")
+                # time.sleep(0.01)
+                self.get_logger().info("while")
                 if self.ser.in_waiting > 0: # to receive 
                     line = self.ser.readline().decode('utf-8').rstrip()
                     self.get_logger().info(line)   
                     self.analize_msg(line)
                     self.ser.reset_input_buffer()
 
-                    
         except KeyboardInterrupt:
             print('close serial')
+            self.get_logger().info("close")
             self.ser.close()
+        self.get_logger().info("done")
     
     def analize_msg(self, line: str):
 
